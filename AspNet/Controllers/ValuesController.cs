6a1d39718e0fa82c3c3df897b5928689f73cc5ca
amerlin <andrea.merlin@aalto.it>
2022-12-09 12:58:38 +0100

    first commit

---
+++ AspNet/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AspNet.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
